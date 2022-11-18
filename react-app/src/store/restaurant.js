4ec2d14b010b567e0bac82dfa4e9c0ac5d6eade4
Mariel <marielvaca@gmail.com>
2022-11-17 21:44:43 -0600

    restaurant store in progress

--- react-app/src/store/restaurant.js
+++ react-app/src/store/restaurant.js
@@ -37,7 +37,7 @@ const removeRestaurant = (restaurantId) => {
 
 //get all restaurants by city
 export const getRestaurantsByCity = (city) => async (dispatch) => {
-    const res = await csrfFetch(`/api/restaurants/${city}`);
+    const res = await fetch(`/api/restaurants/${city}`);
 
     if(res.ok){
         const data = await res.json();
@@ -47,12 +47,48 @@ export const getRestaurantsByCity = (city) => async (dispatch) => {
 };
 
 //get details of a res by url slug
-export const getRestaurant = (restaurantId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/restaurants/${city}`);
+export const getRestaurant = (restaurant_url) => async (dispatch) => {
+    const res = await fetch(`/api/restaurants/${restaurant_url}`);
 
     if(res.ok){
         const data = await res.json();
-        dispatch(setRestaurants(data.restaurants));
+        dispatch(setRestaurant(data));
     }
     return res;
+};
+
+//TODO: ADD AND DELETE THUNK
+
+//Reducer
+export default function restaurantReducer(state = {restaurants: {}, urls: {}}, action){
+    let newState = {...state};
+    switch(action.type){
+        case SET_RESTAURANTS:
+            action.restaurants.forEach((restaurant) => {
+                newState.restaurants[restaurant.id] = restaurant;
+                newState.urls[restaurant.url_slug] = restaurant.id;
+            });
+            break;
+
+        case SET_RESTAURANT:
+            newState.restaurants[action.restaurant.id] = action.restaurant;
+            newState.urls[action.restaurant.url_slug] = action.restaurant.id;
+            break;
+
+        case ADD_RESTAURANT:
+            newState.restaurants[action.restaurant.id] = action.restaurant;
+            newState.urls[action.restaurant.url_slug] = action.restaurant.id;
+            break;
+
+        case DELETE_RESTAURANT:
+            delete newState.restaurants[action.restaurantId];
+            for(let url in newState.urls)
+                if (newState.urls[url_slug] === action.restaurantId)
+                    delete newState.urls[url_slug];
+            break;
+            
+        default:
+            break;
+    }
+    return newState;
 };
