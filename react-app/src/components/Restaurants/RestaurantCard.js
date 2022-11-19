703d06f235e4cd30422a3cf1762f9cf76459a9a8
Mariel <marielvaca@gmail.com>
2022-11-19 17:12:21 -0600

    started card component and added favicon

---
+++ react-app/src/components/Restaurants/RestaurantCard.js
@@ -0,0 +1,28 @@
+function RestaurantCard({restaurant}){
+    return(
+        <div className="restaurant-card">
+             <div className="thumbnail-container">
+                <img src={restaurant.preview_img_url}
+                alt="restaurant image"
+                className="preview-image"
+                />
+            </div>
+
+            <div className="card-details">
+                <Link to={`/restaurants/${restaurant.url_slug}`} className="detail-name">{restaurant.name}</Link>
+                <div>
+                <i class="fa-sharp fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                    {restaurant.rating}
+                </div>
+                <p>{restaurant.type}</p>
+                <p>{restaurant.neighborhood}</p>
+                <p>{restaurant.price_range}</p>
+            </div>
+            <button>reservation</button>
+
+        </div>
+    )
+};
+
+export default RestaurantCard;
